34b46168f2a64cfa1bc758ed7407cb727e261f0a
Artem Belov <otsec.ru@gmail.com>
2021-06-16 18:44:45 +0300

    Increase Next page cache timeouts

---
+++ next.config.js
@@ -0,0 +1,9 @@
+
+module.exports = {
+  onDemandEntries: {
+    // period (in ms) where the server will keep pages in the buffer
+    maxInactiveAge: 60 * 60 * 1000,
+    // number of pages that should be kept simultaneously without being disposed
+    pagesBufferLength: 20,
+  },
+}
