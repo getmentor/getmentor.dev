e7194f72575a28ace81e2596a991215fb6c7669b
Artem Belov <otsec.ru@gmail.com>
2021-06-17 19:19:27 +0300

    Remove Card component

--- src/config/donates.js
+++ src/config/donates.js
@@ -3,8 +3,6 @@ export default [
     name: 'Patreon',
     description: 'Донатить через Patreon',
     linkUrl: 'https://www.patreon.com/getmentor',
-    coverImageUrl:
-      'https://dl.airtable.com/.attachments/24433b517415d9a046694246ca40486a/61d80b5f/patreon.jpg',
     image: {
       url: '/images/patreon.png',
       width: 200,
@@ -15,8 +13,6 @@ export default [
     name: 'Tinkoff',
     description: 'Перевод на карту',
     linkUrl: 'https://www.tinkoff.ru/rm/mogelashvili.georgiy1/llaLa45003',
-    coverImageUrl:
-      'https://dl.airtable.com/.attachments/ddf7a481c48007e1b5064753f3db3236/29648357/512x512bb.jpg',
     image: {
       url: '/images/tinkoff.png',
       width: 200,
@@ -27,8 +23,6 @@ export default [
     name: 'Paypal',
     description: 'Оплатить через PayPal',
     linkUrl: 'https://paypal.me/glamcoder',
-    coverImageUrl:
-      'https://dl.airtable.com/.attachments/1e2b02c5c4dd5ee67ab346f29bd54857/bed675ef/Paypal_2014_logo.png',
     image: {
       url: '/images/paypal.png',
       width: 200,
