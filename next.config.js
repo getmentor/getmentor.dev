baf5d9f4d1280f48981c7e9942983330c56e8cb9
Artem Belov <otsec.ru@gmail.com>
2021-06-20 16:41:24 +0300

    Add cache header for local images

--- next.config.js
+++ next.config.js
@@ -3,6 +3,23 @@ module.exports = {
     domains: ['dl.airtable.com'],
   },
 
+  async headers() {
+    return [
+      // this header fixed bad behaviors of next <Image /> component
+      // now local images from /images directory will be cached for 1 day
+      // otherwise cache image will regenerate every 60 seconds
+      {
+        source: '/images/(.*)',
+        headers: [
+          {
+            key: 'cache-control',
+            value: 'public, max-age=86400, must-revalidate',
+          },
+        ],
+      },
+    ]
+  },
+
   onDemandEntries: {
     // period (in ms) where the server will keep pages in the buffer
     maxInactiveAge: 60 * 60 * 1000,
