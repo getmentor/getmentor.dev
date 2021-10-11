6403328b9e679791109e9e190fff186bf95e3c23
Georgiy Mogelashvili <gmogelashvili@gmail.com>
2021-10-11 19:57:48 +0200

    cors support for mentors API

--- src/pages/api/mentors.js
+++ src/pages/api/mentors.js
@@ -1,8 +1,21 @@
 import * as Sentry from '@sentry/nextjs'
 import { getMentors } from '../../server/cached-mentors'
 import seo from '../../config/seo'
+import Cors from 'cors'
+import initMiddleware from '../../lib/init-middleware'
+
+// Initialize the cors middleware
+const cors = initMiddleware(
+  // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+  Cors({
+    // Only allow requests with GET and OPTIONS
+    methods: ['GET', 'OPTIONS'],
+  })
+)
 
 const handler = async (req, res) => {
+  await cors(req, res)
+
   // Only allow GET
   if (req.method !== 'GET') {
     return res.status(403).json({})
