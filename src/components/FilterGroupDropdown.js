8eb9d9b02ff83d0943647350e2f98789f1c45bbb
Georgiy Mogelashvili <gmogelashvili@gmail.com>
2021-12-17 12:01:31 +0100

    tune up filters

--- src/components/FilterGroupDropdown.js
+++ src/components/FilterGroupDropdown.js
@@ -1,4 +1,6 @@
 import Dropdown from 'rc-dropdown'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronCircleDown } from '@fortawesome/free-solid-svg-icons'
 
 import 'rc-dropdown/assets/index.css'
 
@@ -21,16 +23,20 @@ export default function FilterGroupDropdown({
       },
       button: {
         selected:
-          'text-sm rounded-full py-1 px-4 m-1 cursor-pointer bg-gray-300 text-gray-600 bg-gray-700 text-white',
+          'text-sm rounded-full py-1 pl-4 pr-2 m-1 cursor-pointer bg-gray-300 text-gray-600 bg-gray-700 text-white',
         not_selected:
-          'text-sm rounded-full py-1 px-4 m-1 cursor-pointer bg-gray-300 text-gray-600 bg-gray-300 hover:bg-gray-200 text-gray-600',
+          'text-sm rounded-full py-1 pl-4 pr-2  m-1 cursor-pointer bg-gray-300 text-gray-600 bg-gray-300 hover:bg-gray-200 text-gray-600',
       },
       span: {
         selected: 'mr-2 bg-gray-700 text-white',
         not_selected: 'mr-2 text-gray-600',
       },
       badge:
-        'inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-red-100 bg-red-500 rounded-full',
+        'inline-flex items-center justify-center px-2 py-1 mr-1 text-xs font-bold leading-none text-red-100 bg-red-500 rounded-full',
+      arrow: {
+        selected: 'ml-0 mt-0 text-primary',
+        not_selected: 'ml-0 mt-0',
+      },
     },
     block: {
       ul: 'text-center bg-gray-200 w-max',
@@ -41,16 +47,20 @@ export default function FilterGroupDropdown({
       },
       button: {
         selected:
-          'text-sm rounded-sm py-1 px-4 m-1 cursor-pointer bg-gray-300 text-gray-600 bg-gray-700 text-white',
+          'text-sm rounded-sm py-1 pl-4 pr-2 m-1 cursor-pointer bg-gray-300 text-gray-600 bg-gray-700 text-white',
         not_selected:
-          'text-sm rounded-sm py-1 px-4 m-1 cursor-pointer bg-gray-300 text-gray-600 bg-indigo-200 hover:bg-gray-200 text-gray-600',
+          'text-sm rounded-sm py-1 pl-4 pr-2 m-1 cursor-pointer bg-gray-300 text-gray-600 bg-indigo-200 hover:bg-gray-200 text-gray-600',
       },
       span: {
         selected: 'mr-2 bg-gray-700 text-white',
         not_selected: 'mr-2 text-gray-600',
       },
       badge:
-        'inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-red-100 bg-red-500 rounded-full',
+        'inline-flex items-center justify-center px-2 py-1 mr-1 text-xs font-bold leading-none text-red-100 bg-red-500 rounded-full',
+      arrow: {
+        selected: 'ml-0 mt-0 text-primary',
+        not_selected: 'ml-0 mt-0',
+      },
     },
   }
 
@@ -92,11 +102,18 @@ export default function FilterGroupDropdown({
         >
           {title}
         </span>
-        {multiSelect || selectedValuesCount ? (
+        {selectedValuesCount ? (
           <span className={themes[theme].badge}>{selectedValuesCount}</span>
         ) : (
           <span />
         )}
+        <FontAwesomeIcon
+          icon={faChevronCircleDown}
+          size="1x"
+          className={
+            selectedValuesCount ? themes[theme].arrow.selected : themes[theme].arrow.not_selected
+          }
+        />
       </button>
     </Dropdown>
   )
