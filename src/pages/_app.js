1730e66a4530c7ecf3df2c80fd9a059069088c08
Georgiy Mogelashvili <gmogelashvili@gmail.com>
2022-07-13 16:43:06 +0200

    Use react-gtm for GTM rendering

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
 import '@fortawesome/fontawesome-svg-core/styles.css'
+import { useEffect } from 'react'
+import TagManager from 'react-gtm-module'
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    TagManager.initialize({ gtmId: 'GTM-NBGRPCZ' })
+  }, [])
   return <Component {...pageProps} />
 }
 
