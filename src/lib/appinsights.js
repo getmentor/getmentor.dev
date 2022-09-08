48ed697cda10c0f4fabf09bff943ae9e8e486383
Georgiy Mogelashvili <gmogelashvili@gmail.com>
2022-09-08 09:20:49 +0200

    add appinsights for react

---
+++ src/lib/appinsights.js
@@ -0,0 +1,31 @@
+import { ApplicationInsights } from '@microsoft/applicationinsights-web'
+import { ReactPlugin } from '@microsoft/applicationinsights-react-js'
+
+const defaultBrowserHistory = {
+  url: '/',
+  location: { pathname: '' },
+  listen: () => {},
+}
+
+let browserHistory = defaultBrowserHistory
+if (typeof window !== 'undefined') {
+  browserHistory = { ...browserHistory, ...window.history }
+  browserHistory.location.pathname = browserHistory?.state?.url
+}
+
+var reactPlugin = new ReactPlugin()
+var appInsights = new ApplicationInsights({
+  config: {
+    connectionString: process.env.NEXT_PUBLIC_APPLICATIONINSIGHTS_CONNECTION_STRING,
+    extensions: [reactPlugin],
+    extensionConfig: {
+      [reactPlugin.identifier]: { history: browserHistory },
+    },
+  },
+})
+
+if (typeof window !== 'undefined') {
+  appInsights.loadAppInsights()
+}
+
+export { appInsights, reactPlugin }
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,12 +2,18 @@ import '../styles/globals.css'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import { useEffect } from 'react'
 import TagManager from 'react-gtm-module'
+import { AppInsightsContext } from '@microsoft/applicationinsights-react-js'
+import { reactPlugin } from '../lib/appinsights'
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
     TagManager.initialize({ gtmId: 'GTM-NBGRPCZ' })
   }, [])
-  return <Component {...pageProps} />
+  return (
+    <AppInsightsContext.Provider value={reactPlugin}>
+      <Component {...pageProps} />
+    </AppInsightsContext.Provider>
+  )
 }
 
 export default MyApp
