0de089a8d6860cafb614728cd443d41bf9f01665
Georgiy Mogelashvili <gmogelashvili@gmail.com>
2022-09-12 20:00:18 +0200

    healthcheck no cache

--- src/pages/api/healthcheck.js
+++ src/pages/api/healthcheck.js
@@ -1,6 +1,7 @@
 import { withSentry } from '@sentry/nextjs'
 
 const handler = (req, res) => {
+  res.setHeader('Cache-Control', 'no-cache, no-store, max-age=0, must-revalidate')
   res.status(200).json({})
 }
 
