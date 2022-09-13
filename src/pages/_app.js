4e58a1aa8da55d222eda74b5faeb25f2efdfe64d
Georgiy Mogelashvili <gmogelashvili@gmail.com>
2022-09-13 12:16:36 +0200

    remove appsignal

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -17,12 +17,3 @@ function MyApp({ Component, pageProps }) {
 }
 
 export default MyApp
-
-export function reportWebVitals(metric) {
-  const body = JSON.stringify(metric)
-  const url = '/__appsignal-web-vitals'
-
-  // Use `navigator.sendBeacon()` if available, falling back to `fetch()`.
-  ;(navigator.sendBeacon && navigator.sendBeacon(url, body)) ||
-    fetch(url, { body, method: 'POST', keepalive: true })
-}
