2c34c2e6f8ed59e4caa0e4677aed419a11c732b1
Georgiy Mogelashvili <gmogelashvili@gmail.com>
2022-09-13 11:52:18 +0200

    remove healthcheck2

--- src/api/healthcheck2.js
+++
@@ -1,8 +0,0 @@
-import { withSentry } from '@sentry/nextjs'
-
-const handler = (req, res) => {
-  res.setHeader('Cache-Control', 'no-cache, no-store, max-age=0, must-revalidate')
-  res.status(200).json({})
-}
-
-export default withSentry(handler)
