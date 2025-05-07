5e1d09e43cec019e58a38c8c0d31aef41342af19
Georgiy Mogelashvili <gmogelashvili@gmail.com>
2025-05-07 20:18:55 +0200

    sitch to table layout

--- src/pages/mentors_aikb.js
+++ src/pages/mentors_aikb.js
@@ -25,34 +25,34 @@ export default function MentorsAIKB({ pageMentors }) {
         <title>{seo.title}</title>
       </Head>
 
-      {pageMentors?.map((mentor) => (
-        <div key={mentor.id}>
-          <h2>{mentor.name}</h2>
-          <p>
-            {mentor.job} @ {mentor.workplace}
-          </p>
-          <p>
-            Описание:
-            <pre>{mentor.about}</pre>
-          </p>
-          <p>
-            Чем помогу:
-            <pre>{mentor.description}</pre>
-          </p>
-          <p>
-            Компетенции: <pre>{mentor.competencies}</pre>
-          </p>
-          <p>Теги: {mentor.tags}</p>
-          <p>Опыт: {mentor.experience}</p>
-          <p>Цена: {mentor.price}</p>
-          <p>Помог людям: {mentor.menteeCount}</p>
-          <p>Новый на сайте: {mentor.isNew}</p>
-          <p>Фото: {imageLoader({ src: mentor.slug, quality: 'large' })}</p>
-          <p>
-            Ссылка на профиль: {seo.domain}/mentor/{mentor.slug}
-          </p>
-        </div>
-      ))}
+      <table>
+        <tr>
+          <th>Имя</th>
+          <th>Должность и работа</th>
+          <th>Описание</th>
+          <th>С чем может помочь</th>
+          <th>Компетенции</th>
+          <th>Теги</th>
+          <th>Опыт</th>
+          <th>Цена</th>
+          <th>Ссылка на фото</th>
+          <th>ССылка на профиль</th>
+        </tr>
+        {pageMentors?.map((mentor) => (
+          <tr key={mentor.id}>
+            <td>{mentor.name}</td>
+            <td>{mentor.job + ' @ ' + mentor.workplace}</td>
+            <td>{mentor.about}</td>
+            <td>{mentor.description}</td>
+            <td>{mentor.competencies}</td>
+            <td>{mentor.tags.join(',')}</td>
+            <td>{mentor.experience}</td>
+            <td>{mentor.price}</td>
+            <td>{imageLoader({ src: mentor.slug, quality: 'large' })}</td>
+            <td>{seo.domain + '/mentor/' + mentor.slug}</td>
+          </tr>
+        ))}
+      </table>
     </>
   )
 }
