e19e944104dc435d2185f540ee1d130596c9be87
Georgiy Mogelashvili <gmogelashvili@gmail.com>
2025-10-16 17:24:37 +0200

    fix iframe code

--- src/components/CalendlabWidget.js
+++ src/components/CalendlabWidget.js
@@ -1,14 +1,5 @@
 export default function CalendlabWidget({ url }) {
   const embedUrl = url + '?embed=true'
 
-  return (
-    <iframe
-      src={embedUrl}
-      width="100%"
-      height="700"
-      frameBorder="0"
-      style="border: none; min-height: 700px;"
-      allowfullscreen
-    ></iframe>
-  )
+  return <iframe src={embedUrl} width="100%" height="700px" frameBorder="0"></iframe>
 }
