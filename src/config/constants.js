0ff4d5897679a90e25b33c3760c034b080f5ea5d
Georgiy Mogelashvili <gmogelashvili@gmail.com>
2025-11-16 13:06:51 +0100

    domain

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,7 +1,7 @@
 export default {
   BASE_URL: {
     development: 'http://localhost:3000/',
-    production: 'https://getmentor.dev/',
+    production: 'https://гетментор.рф/',
   }[process.env.NODE_ENV],
 
   CACHE_REFRESH_INTERVAL: 1 * 60 * 1000,
