1f5d8e11d99645ba6745964437786ea4d8e26c27
Georgiy Mogelashvili <gmogelashvili@gmail.com>
2025-11-20 16:57:40 +0100

    domain url fix

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,7 +1,7 @@
 export default {
   BASE_URL: {
     development: 'http://localhost:3000/',
-    production: 'https://гетментор.рф/',
+    production: `https://${process.env.DOMAIN}/`,
   }[process.env.NODE_ENV],
 
   CACHE_REFRESH_INTERVAL: 1 * 60 * 1000,
