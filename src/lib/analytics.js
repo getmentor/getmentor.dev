990fdf9cdb94eacaf5b1304ac29b261fc2972ae8
Georgiy Mogelashvili <gmogelashvili@gmail.com>
2025-11-27 18:05:58 +0100

    removed amplitude

--- src/lib/analytics.js
+++ src/lib/analytics.js
@@ -1,7 +1,6 @@
 export default {
   event(name, params) {
     if (typeof window !== 'undefined') {
-      window?.amplitude?.getInstance().logEvent(name, params)
       window?.mixpanel?.track(name, params)
     }
   },
